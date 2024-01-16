68be8a674d6b84d578b00a8d83a063b5ddef82d1
hongdiem_roro <mingukmanse673@gmail.com>
2024-01-16 14:27:44 +0700

    form login responsive

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from "./logo.svg";
+import Nav from "./components/Navigation/Nav";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.scss";
-
+import Login from "./components/Login/Login";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello world React with Hoi Dan IT</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-container">
+        <Nav />
+        <Switch>
+          <Route path="/about">About</Route>
+          <Route path="/news">News</Route>
+          <Route path="/contact">Contact</Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/" exact>
+            Home
+          </Route>
+          <Route path="*">404 NOT FOUND</Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Login/Login.js
@@ -0,0 +1,45 @@
+import "./Login.scss";
+
+function Login() {
+  return (
+    <div className="login-container">
+      <div className="container">
+        <div className="row px-3 px-sm-0">
+          <div className="content-left col-12 d-none col-sm-7 d-sm-block">
+            <div className="brand">facebook</div>
+            <div className="detail">
+              Facebook helps you connect and share with the people in your life.
+            </div>
+          </div>
+          <div className="content-right col-12 col-sm-5 d-flex flex-column gap-3 py-3">
+            <div className="brand d-sm-none d-flex justify-content-center">
+              facebook
+            </div>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Email address or your phone number"
+            />
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+            />
+            <button className="btn btn-primary">Login</button>
+            <span className="text-center">
+              <a className="forgotten-password" href="#">
+                Forgotten password?
+              </a>
+            </span>
+            <hr />
+            <div className="text-center">
+              <button className="btn btn-success">Create new account</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
